915883549ca728376f5fc3011978af4206bdd807
Nahuel Pelli <nahuelpelli91@gmail.com>
2025-01-08 16:03:10 +0100

    Adding more configs for neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set multiple color columns
+vim.opt.colorcolumn = "80,100"
+
+-- Highlight the color columns with a subtle background color
+vim.cmd [[
+  highlight ColorColumn ctermbg=lightgrey guibg=#303030
+]]
+
